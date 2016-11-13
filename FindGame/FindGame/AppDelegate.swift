c2a604b87e67b72d9abef889200dc1b422349136
Samuel Zugasti <sam.zugasti@nike.com>
2016-11-12 21:10:53 -0800

    added firebase to project

--- FindGame/FindGame/AppDelegate.swift
+++ FindGame/FindGame/AppDelegate.swift
@@ -2,6 +2,7 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -11,6 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        FIRApp.configure()
         return true
     }
 
